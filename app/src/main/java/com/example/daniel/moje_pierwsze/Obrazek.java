f191f758815a0b02fe781e18da992479a3b3d206
Zginom <chomik13.poczta@o2.pl>
2017-04-03 21:13:34 +0200

    zadanie3

--- app/src/main/java/com/example/daniel/moje_pierwsze/Obrazek.java
+++ app/src/main/java/com/example/daniel/moje_pierwsze/Obrazek.java
@@ -1,8 +1,13 @@
 package com.example.daniel.moje_pierwsze;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Obrazek extends AppCompatActivity {
 
@@ -10,11 +15,25 @@ public class Obrazek extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obrazek);
+    }
 
+    public void nastepny( View v )
+    {
+        ImageView imageView = (ImageView)findViewById(R.id.android2);
+        imageView.setImageResource(R.mipmap.android);
+    }
 
-        Button b1 = (Button)findViewById(R.id.button1);
-        Button b2 = (Button)findViewById(R.id.button2);
-        Button b3 = (Button)findViewById(R.id.button3);
+    public void poprzedni( View v )
+    {
+        ImageView imageView = (ImageView)findViewById(R.id.android2);
+        imageView.setImageResource(R.mipmap.android2);
+    }
 
+    public void powrot( View v)
+    {
+        Context cont;
+        cont = getApplicationContext();
+        Intent intent = new Intent(cont,MainActivity.class);
+        startActivity(intent);
     }
 }
